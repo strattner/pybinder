165ba70a4203229446c90d3235a230574108588b
Scott Strattner <sstrattn@us.ibm.com>
2017-05-30 14:50:34 -0400

    Initial commit, upload to ibm github

---
+++ modifydns.py
@@ -0,0 +1,271 @@
+#!/usr/bin/python3
+"""
+ modifydns
+
+ Author: Scott Strattner (sstrattn@us.ibm.com)
+
+ Copyright (c) 2017 IBM Corp.
+ All Rights Reserved
+
+"""
+
+import re
+import logging
+import ipaddress
+import dns.query
+import dns.tsigkeyring
+import dns.update
+import dns.rdatatype
+import dns.reversename
+from docopt import docopt
+
+
+class DNSModifyError(Exception):  # pylint: disable=missing-docstring
+    NO_ZONE = "No zone specififed"
+
+
+class ModifyDNS(object):
+    """
+    Perform atomic DNS add and delete actions.
+    """
+
+    TTL = 86400
+
+    def __init__(self,  # pylint: disable=too-many-arguments
+                 nameserver=None,
+                 forward_zone=None,
+                 reverse_zone=None,
+                 ttl=None,
+                 key_name=None,
+                 key_hash=None):
+        self.nameserver = nameserver
+        self.forward_zone = forward_zone
+        self.reverse_zone = reverse_zone
+        self.ttl = ttl if ttl else self.TTL
+        if key_name and key_hash:
+            self.keyring = dns.tsigkeyring.from_text({key_name: key_hash})
+        else:
+            self.keyring = None
+        self.__print_attributes()
+
+    def __print_attributes(self):
+        logging.debug("%s", self.__dict__)
+
+    def __get_name_and_zone(self, name):
+        if '.' in name:
+            (shortname, zone) = name.split('.', 1)
+        else:
+            shortname = name
+            if self.forward_zone:
+                zone = self.forward_zone
+            else:
+                logging.debug("Unable to find zone for %s", name)
+                raise DNSModifyError(DNSModifyError.NO_ZONE)
+        return (shortname, zone)
+
+    def __get_rev_name_and_zone(self, address):
+        zone = self.reverse_zone
+        revname = dns.reversename.from_address(address)
+        if not zone:  # Without a given reverse zone, the assumption is that the zone
+            # is at the "class C" aka /24 boundary
+            try:
+                network_address = ipaddress.ip_network(address + '/24')
+                rev_network_addr = str(
+                    dns.reversename.from_address(str(network_address)))
+                last_octet_index = rev_network_addr.find('.')
+                zone = rev_network_addr[last_octet_index:]
+                print(zone)
+            except ValueError:
+                logging.debug("Unable to find reverse zone for %s", address)
+                raise DNSModifyError(DNSModifyError.NO_ZONE)
+        return (revname, zone)
+
+    def add_forward(self, name, address):
+        """
+        Create A record. Does not check if name is already in use.
+        Does not verify that address is an actual IP address.
+        """
+        logging.debug("Forward add request for %s %s", name, address)
+        shortname, zone = self.__get_name_and_zone(name)
+        update = dns.update.Update(zone, keyring=self.keyring)
+        update.add(shortname, self.ttl, dns.rdatatype.A, address)
+        result = dns.query.tcp(update, self.nameserver)
+        logging.debug("Adding forward record result: %s", result)
+        return result
+
+    def add_rr(self, name, address):
+        """
+        Round-robin record is just multiple A records with same name.
+        """
+        result = []
+        for addr in address:
+            result.append(self.add_forward(name, addr))
+        return result
+        
+
+    def add_reverse(self, address, name):
+        """
+        Create PTR record. Does not check if address is already in use.
+        """
+        logging.debug("Reverse add request for %s %s", address, name)
+        reverse_name, zone = self.__get_rev_name_and_zone(address)
+        shortname, fzone = self.__get_name_and_zone(name)
+        fqdn = shortname + '.' + fzone
+        if not fqdn.endswith('.'):
+            fqdn += '.'
+        update = dns.update.Update(zone, keyring=self.keyring)
+        update.add(reverse_name, self.ttl, dns.rdatatype.PTR, fqdn)
+        result = dns.query.tcp(update, self.nameserver)
+        logging.debug("Adding reverse record result: %s", result)
+        return result
+
+    def add_alias(self, cname, rname):
+        """
+        Create CNAME record. Does not check if cname is already in use, or if
+        rname exists.
+        """
+        logging.debug("Alias add request for %s pointing to %s", cname, rname)
+        cshort, czone = self.__get_name_and_zone(cname)
+        rshort, rzone = self.__get_name_and_zone(rname)
+        rfqdn = rshort + '.' + rzone
+        if not rfqdn.endswith('.'):
+            rfqdn += '.'
+        update = dns.update.Update(czone, keyring=self.keyring)
+        update.add(cshort, self.ttl, dns.rdatatype.CNAME, rfqdn)
+        result = dns.query.tcp(update, self.nameserver)
+        logging.debug("Adding alias record result: %s", result)
+        return result
+
+    def delete_forward(self, name):
+        logging.debug("Forward delete request for %s", name)
+        shortname, zone = self.__get_name_and_zone(name)
+        update = dns.update.Update(zone, keyring=self.keyring)
+        update.delete(shortname, dns.rdatatype.A)
+        result = dns.query.tcp(update, self.nameserver)
+        logging.debug("Deleting forward record result: %s", result)
+        return result
+
+    def delete_reverse(self, address):
+        logging.debug("Reverse delete request for %s", address)
+        reverse_name, zone = self.__get_rev_name_and_zone(address)
+        update = dns.update.Update(zone, keyring=self.keyring)
+        update.delete(reverse_name, dns.rdatatype.PTR)
+        result = dns.query.tcp(update, self.nameserver)
+        logging.debug("Deleting reverse record result: %s", result)
+        return result
+
+    def delete_alias(self, alias):
+        logging.debug("Alias delete request for %s", alias)
+        shortname, zone = self.__get_name_and_zone(alias)
+        update = dns.update.Update(zone, keyring=self.keying)
+        update.delete(shortname, dns.rdatatype.CNAME)
+        result = dns.query.tcp(update, self.nameserver)
+        logging.debug("Deleting alias record result: %s", result)
+        return result
+
+def parse_key_file(kfile):
+    """
+    Given a TSIG key file (as regenerated by rndc), extract the key name and hash.
+    """
+    with open(kfile) as keyf:
+        contents = keyf.readlines()
+    for kline in contents:
+        key_name_result = re.search(r'key (\S+) ', kline)
+        if key_name_result:
+            key_name = key_name_result.group(1)
+        key_hash_result = re.search(r'secret \"(\S+)\"', kline)
+        if key_hash_result:
+            key_hash = key_hash_result.group(1).rstrip()
+    return (key_name, key_hash)
+
+def main():
+    """
+    ModifyDNS
+
+    Usage:
+        modifydns.py add_forward [--debug <dfile>] [--server <server>] [--key <kfile>]
+                                 [--zone <zone>] <name> <address>
+        modifydns.py add_reverse [--debug <dfile>] [--server <server>] [--key <kfile>]
+                                 [--zone <zone>] <address> <name>
+        modifydns.py add_alias [--debug <dfile>] [--server <server>] [--key <kfile>]
+                               [--zone <zone>] <alias> <name>
+        modifydns.py add_rr [--debug <dfile>] [--server <server>] [--key <kfile>]
+                            [--zone <zone>] <name> <address>...
+        modifydns.py delete_forward [--debug <dfile>] [--server <server>] [--key <kfile>]
+                                 [--zone <zone>] <name>
+        modifydns.py delete_reverse [--debug <dfile>] [--server <server>] [--key <kfile>]
+                                 [--zone <zone>] <address>
+        modifydns.py delete_alias [--debug <dfile>] [--server <server>] [--key <kfile>]
+                               [--zone <zone>] <alias>
+        modifydns.py -h | --help
+
+    Arguments:
+        name        FQDN or shortname (if zone can be determined)
+        address     IP address
+        alias       FQDN or shortname (if zone can be determined)
+
+    Options:
+        -h --help        Show this screen
+        --debug <file>     Send debug logs to file
+        --server <server>  Specify nameserver, if different from system default
+        --key <kfile>      The TSIG key file containing name and hash
+        --zone <zone>      Forward or reverse zone, if overriding name/address zone
+    """
+    arguments = docopt(str(main.__doc__))
+    if '--debug' in arguments:
+        print("Running in debug mode to {}".format(arguments['--debug']))
+        log_date = '%Y-%m-%d %H:%M:%S'
+        log_form = '%(asctime)s %(message)s'
+        logging.basicConfig(
+            filename=arguments['--debug'],
+            level=logging.DEBUG,
+            format=log_form,
+            datefmt=log_date)
+    logging.debug("Arguments: %s", arguments)
+    zone = arguments['--zone'] if '--zone' in arguments else None
+    server = arguments['--server'] if '--server' in arguments else None
+    name = arguments['<name>']
+    address = arguments['<address>']
+    alias = arguments['<alias>']
+    if '--key' in arguments:
+        (key_name, key_hash) = parse_key_file(arguments['--key'])
+    else:
+        key_name = key_hash = None
+    if arguments['add_reverse'] or arguments['delete_reverse']:
+        modifier = ModifyDNS(server, None, zone, None, key_name, key_hash)
+    else:
+        modifier = ModifyDNS(server, zone, None, None, key_name, key_hash)
+    result = []
+    try:
+        if arguments['add_forward']:
+            address = address.pop(0)
+            logging.debug("Request to add forward entry for %s %s", name, address)
+            result.append(modifier.add_forward(name, address))
+        if arguments['add_reverse']:
+            address = address.pop(0)
+            logging.debug("Request to add reverse entry for %s %s", address, name)
+            result.append(modifier.add_reverse(address, name))
+        if arguments['add_alias']:
+            logging.debug("Request to add alias for %s %s", alias, name)
+            result.append(modifier.add_alias(alias, name))
+        if arguments['add_rr']:
+            logging.debug("Request to add round-robin for %s %s", name, address)
+            result.extend(modifier.add_rr(name, address))
+        if arguments['delete_forward']:
+            logging.debug("Request to delete forward entry %s", name)
+            result.append(modifier.delete_forward(name))
+        if arguments['delete_reverse']:
+            address = address.pop(0)
+            logging.debug("Request to delete reverse entry %s", address)
+            result.append(modifier.delete_reverse(address))
+        if arguments['delete_alias']:
+            logging.debug("Request to delete alias entry %s", alias)
+            result.append(modifier.delete_alias(alias))
+    except DNSModifyError as dme:
+        print("Error encountered: {}".format(str(dme)))
+    for res in result:
+        print(res)
+
+
+if __name__ == "__main__":
+    main()
---
+++ searchdns.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python3
+"""
+ searchdns
+
+ Query one or more records in DNS, using either system default or
+ specified nameserver and/or search domain.
+
+ Author: Scott Strattner (sstrattn@us.ibm.com)
+
+ Copyright (c) 2017 IBM Corp.
+ All Rights Reserved
+
+"""
+import re
+import logging
+import ipaddress
+import dns.resolver
+import dns.name
+import dns.reversename
+from docopt import docopt
+
+
+class DNSSearchAnswerError(Exception):  # pylint: disable=missing-docstring
+    pass
+
+
+class DNSSearchAnswer(object):  # pylint: disable=too-few-public-methods
+    """
+    Parses and stores the answer to a SearchDNS query.
+    Only understands A, PTR, and CNAME records.
+    """
+
+    FORWARD = "A"
+    REVERSE = "PTR"
+    ALIAS = "CNAME"
+    ROUND_ROBIN = "Round robin"
+    NOT_FOUND = "not found in DNS"
+
+    def __init__(self, entry, value=None):
+        logging.debug("Inserting DNSSearchAnswer %s %s", entry, value)
+        self.entry = entry
+        if not value:
+            self.type = DNSSearchAnswer.NOT_FOUND
+            return None
+        self.__parse_answer(value)
+
+    def __parse_answer(self, answer):
+        first_result = str(answer.pop(0))
+        logging.debug("Parsing first result: %s", first_result)
+        result_type_search = re.search(r'IN (\S+) (\S+)$', first_result)
+        result_type = result_type_search.group(1)
+        result_answer = result_type_search.group(2)
+        logging.debug("Result type %s with value %s", result_type,
+                      result_answer)
+        if result_type == DNSSearchAnswer.ALIAS:
+            self.type = DNSSearchAnswer.ALIAS
+            self.real_name = result_answer
+        elif result_type == DNSSearchAnswer.FORWARD:
+            self.type = DNSSearchAnswer.FORWARD
+            self.addr = result_answer
+            if answer:
+                self.addr = [self.addr]
+                self.type = DNSSearchAnswer.ROUND_ROBIN
+                for ans in answer:
+                    next_ip_search = re.search(r'IN A (\S+)$', ans)
+                    self.addr.append(next_ip_search.group(1))
+        elif result_type == DNSSearchAnswer.REVERSE:
+            self.type = DNSSearchAnswer.REVERSE
+            self.name = result_answer
+        else:
+            raise DNSSearchAnswerError(
+                "Unrecognized DNS response: {}".format(answer))
+        return True
+
+    def __str__(self):
+        if self.type == DNSSearchAnswer.NOT_FOUND:
+            return self.entry + " " + self.type
+        return_string = self.entry + " "
+        if self.type == DNSSearchAnswer.ALIAS:
+            return_string += "(alias for) " + self.real_name
+        if self.type == DNSSearchAnswer.ROUND_ROBIN:
+            return_string += " ".join(self.addr)
+        if self.type == DNSSearchAnswer.FORWARD:
+            return_string += self.addr
+        if self.type == DNSSearchAnswer.REVERSE:
+            return_string += self.name
+        return return_string
+
+
+class SearchDNS(object):
+    """
+    Performs DNS query against system or specified nameserver,
+    returns results as DNSSearchAnswer instance.
+    """
+
+    FORWARD = "A"
+    REVERSE = "PTR"
+    ALIAS = "CNAME"
+    NORMAL = [FORWARD, REVERSE]
+
+    def __init__(self, nameserver=None, zone=None):
+        self.nameserver = nameserver
+        self.zone = zone
+        self.searcher = dns.resolver.Resolver()
+        self.searcher.set_flags(0)  # do not perform recursive query
+        if self.nameserver:
+            logging.debug("Using %s as nameserver", self.nameserver)
+            self.searcher.nameservers = [self.nameserver]
+        if self.zone:
+            logging.debug("Using %s as default domain", self.zone)
+            self.searcher.search = [dns.name.from_text(self.zone)]
+
+    @staticmethod
+    def is_address(entry):  # pylint: disable=missing-docstring
+        try:
+            _ = ipaddress.ip_address(entry)
+            return True
+        except ValueError:
+            return False
+
+    def query(self, entry, search_type=None):  # pylint: disable=missing-docstring
+        logging.debug("Performing a search for %s", entry)
+        if not search_type:
+            if SearchDNS.is_address(entry):
+                search_type = SearchDNS.REVERSE
+                search_entry = dns.reversename.from_address(entry)
+            else:
+                search_type = SearchDNS.FORWARD
+                search_entry = entry
+        try:
+            answer = self.searcher.query(search_entry, search_type).response
+            answer_list = str(answer.answer.pop(0)).splitlines()
+            logging.debug("Answer: %s", answer_list)
+            return DNSSearchAnswer(entry, answer_list)
+        except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
+            return DNSSearchAnswer(entry)
+
+
+def main():
+    """
+    SearchDNS.
+
+    Usage:
+           searchdns.py [--server <server>] [--domain <domain>] [--debug <file>] <query>...
+           searchdns.py -h | --help
+
+    Arguments:
+        query                   One or more names or IPs to search
+
+    Options:
+        -h --help               Show this screen
+        --server <server>       Specify the nameserver (if not system default)
+        --domain <domain>       Specify the search domain (if FQDN is not given)
+        --debug <file>          Send debug messages to a file
+    """
+
+    arguments = docopt(str(main.__doc__))
+    if arguments['--debug']:
+        print("Running in debug mode to {}".format(arguments['--debug']))
+        log_date = '%Y-%m-%d %H:%M:%S'
+        log_form = '%(asctime)s %(message)s'
+        logging.basicConfig(
+            filename=arguments['--debug'],
+            level=logging.DEBUG,
+            format=log_form,
+            datefmt=log_date)
+        logging.debug("Program arguments: %s", arguments)
+    nameserver = arguments['--server'] if '--server' in arguments else None
+    domain = arguments['--domain'] if '--domain' in arguments else None
+    searcher = SearchDNS(nameserver, domain)
+    for query in arguments['<query>']:
+        print(searcher.query(query))
+
+
+if __name__ == "__main__":
+    main()
